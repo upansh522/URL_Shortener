c337da4c203282ba758a1c87630909e77fcb0e14
[Nishant Upadhyay] <nishantupadhyay813@gmail.com>
2024-06-24 01:02:45 +0530

    urlSchema created

---
+++ models/url.js
@@ -0,0 +1,20 @@
+const mongoose=require("mongoose");
+
+const urlSchema= new mongoose.Schema({
+    shortUrl: {
+        type: string,
+        required: true,
+        unique: true
+    },
+
+    redierctUrl: {
+        type:string,
+        required: true
+    },
+
+    visitHistory:[{
+        timestamp: {type: Number}
+    }]
+},
+{timestamp: true}
+)
