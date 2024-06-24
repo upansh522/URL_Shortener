fcc3224203ead2239c865c81e5ce9b046055e70f
[Nishant Upadhyay] <nishantupadhyay813@gmail.com>
2024-06-25 02:50:34 +0530

    Port Change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const connection=require("./connection");
 const router = require("./routes/url");
 
 const app=express();
-const PORT= 8000;
+const PORT= 8080;
 app.use(express.json());
 
 connection("mongodb://127.0.0.1:27017/UrlShortner")
