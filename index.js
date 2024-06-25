17e4db72e63c2e879034d2aefdcf006d200cdd3c
[Nishant Upadhyay] <nishantupadhyay813@gmail.com>
2024-06-26 01:37:23 +0530

    Primary views setup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express =require("express");
 const connection=require("./connection");
 const router = require("./routes/url");
+const path =require("path");
+const { Url } = require("./models/url");
 
 const app=express();
 const PORT= 8080;
@@ -11,6 +13,16 @@ connection("mongodb://127.0.0.1:27017/UrlShortner")
 ).catch((err)=>console.log("MongoDb Not Connected :",err));
 
 app.use("/url",router)
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set("views", path.resolve("./views"));
+ 
+app.get("/test",async (req,res)=>{
+    const allUsers= await Url.find({});
+    return res.render('home',{
+        urls: allUsers
+    });
+});
 
 app.listen(PORT,()=>{
     console.log("Server Get Started at PORT: ",PORT);
